ea8d283296701dd01b9111d6cd0ac2740fad98c8
debanjan maji <debanjanmaji700@gmail.com>
2024-06-20 10:42:11 +0530

    nothing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import RandomQuote from './assets/RandomQuote'
+
+function App() {
+  return (
+    <div >
+    <RandomQuote/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/RandomQuote.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './RandomQuote.css'
+const RandomQuote = () => {
+
+    let quotes = [];
+
+    async function loadquotes(){
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+
+    const random = () => {
+       const select =quotes[Math.floor(Math.random()*quotes.length)]
+       setQuote(select);
+    }
+
+    const [quote,setQuote]=useState({
+        text:"Difficulties increase the nearer we get to the goal .",
+        author:"Johann Wolfagang von Goethe",
+    });
+    loadquotes();
+
+  return (
+    <div className='container'>
+        <div className='quote'>{quote.text} </div>
+        <div>
+            <div className='line'></div>
+            <div className='bottom'>
+                <div className='author'>{quote.author}</div>
+                <button type="button" class="button">
+  <span class="button__text" onClick={()=>{random()}}>Refresh</span>
+  <span class="button__icon" onClick={()=>{random()}}><svg class="svg" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M35.3 12.7c-2.89-2.9-6.88-4.7-11.3-4.7-8.84 0-15.98 7.16-15.98 16s7.14 16 15.98 16c7.45 0 13.69-5.1 15.46-12h-4.16c-1.65 4.66-6.07 8-11.3 8-6.63 0-12-5.37-12-12s5.37-12 12-12c3.31 0 6.28 1.38 8.45 3.55l-6.45 6.45h14v-14l-4.7 4.7z"></path><path d="M0 0h48v48h-48z" fill="none"></path></svg></span>
+</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RandomQuote
